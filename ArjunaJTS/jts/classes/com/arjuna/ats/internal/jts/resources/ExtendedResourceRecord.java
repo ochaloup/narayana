68e5212a175ca82605c87c702318e390d3b06042
Tom Jenkinson <tom.jenkinson@redhat.com>
2012-01-18 18:10:45 +0000

    JBTM-1021

--- ArjunaJTS/jts/classes/com/arjuna/ats/internal/jts/resources/ExtendedResourceRecord.java
+++ ArjunaJTS/jts/classes/com/arjuna/ats/internal/jts/resources/ExtendedResourceRecord.java
@@ -435,8 +435,10 @@ public class ExtendedResourceRecord extends com.arjuna.ats.arjuna.coordinator.Ab
             return TwoPhaseOutcome.HEURISTIC_MIXED;
         } catch (HeuristicHazard e4) {
             return TwoPhaseOutcome.HEURISTIC_HAZARD;
-        } catch (SystemException e5) {
-            jtsLogger.i18NLogger.warn_resources_errgenerr("ExtendedResourceRecord.topLevelCommit", e5);
+        } catch (OBJECT_NOT_EXIST e5) {
+            jtsLogger.i18NLogger.warn_1pc_commit_one();
+        } catch (SystemException e6) {
+            jtsLogger.i18NLogger.warn_resources_errgenerr("ExtendedResourceRecord.topLevelCommit", e6);
 
             return TwoPhaseOutcome.FINISH_ERROR;
         }
