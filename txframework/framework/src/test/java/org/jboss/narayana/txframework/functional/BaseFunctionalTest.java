fd5d524ae91b5743ea5763b1d6104e1a2d172fd2
Paul Robinson <paul.robinson@redhat.com>
2012-02-07 16:24:34 +0000

    JBTM-1050, JBTM-1002, DEFAULT and NONE bridge type support on @WSAT

--- txframework/framework/src/test/java/org/jboss/narayana/txframework/functional/BaseFunctionalTest.java
+++ txframework/framework/src/test/java/org/jboss/narayana/txframework/functional/BaseFunctionalTest.java
@@ -12,7 +12,8 @@ public class BaseFunctionalTest {
     public static JavaArchive createTestArchive() {
         // todo: Does the application developer have to specify the interceptor?
         JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "test.jar")
-                .addPackages(true, "org.jboss.narayana.txframework").addAsResource("context-handlers.xml")
+                .addPackages(true, "org.jboss.narayana.txframework").addAsResource("jaxws-handlers-jaxws-service.xml")
+                .addAsManifestResource("persistence.xml")
                 .addAsManifestResource(new ByteArrayAsset(
                         "<interceptors><class>org.jboss.narayana.txframework.impl.ServiceRequestInterceptor</class></interceptors>"
                                 .getBytes()),
@@ -21,7 +22,7 @@ public class BaseFunctionalTest {
         archive.delete(ArchivePaths.create("META-INF/MANIFEST.MF"));
 
         String ManifestMF = "Manifest-Version: 1.0\n"
-                + "Dependencies: org.jboss.xts,org.jboss.modules,deployment.arquillian-service,org.jboss.msc,org.jboss.jts\n";
+                + "Dependencies: org.jboss.as.xts,org.jboss.xts,org.jboss.modules,deployment.arquillian-service,org.jboss.msc,org.jboss.jts\n";
         archive.setManifest(new StringAsset(ManifestMF));
 
         return archive;
