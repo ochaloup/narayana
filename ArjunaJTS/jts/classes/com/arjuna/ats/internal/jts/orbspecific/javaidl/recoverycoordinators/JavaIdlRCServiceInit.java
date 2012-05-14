f35a35b800da3dd9fde86333c7036851a17d5d79
mmusgrov <mmusgrov@redhat.com>
2012-05-14 11:05:13 +0100

    [JBTM934] Specify ORBServerId programatically

--- ArjunaJTS/jts/classes/com/arjuna/ats/internal/jts/orbspecific/javaidl/recoverycoordinators/JavaIdlRCServiceInit.java
+++ ArjunaJTS/jts/classes/com/arjuna/ats/internal/jts/orbspecific/javaidl/recoverycoordinators/JavaIdlRCServiceInit.java
@@ -124,6 +124,7 @@ public class JavaIdlRCServiceInit implements RecoveryServiceInit {
 
             final Properties p = new Properties();
             p.setProperty("com.sun.CORBA.POA.ORBPersistentServerPort", recoveryManagerPort);
+            p.setProperty("com.sun.CORBA.POA.ORBServerId", recoveryManagerPort);
 
             if (recoveryManagerAddr != null && recoveryManagerAddr.length() > 0) {
                 p.setProperty("OAIAddr", recoveryManagerAddr);
