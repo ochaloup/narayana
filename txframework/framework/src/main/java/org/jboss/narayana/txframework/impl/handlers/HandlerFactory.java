365160174ed59ab6a2c5b14e68f28298a75e7269
Paul Robinson <probinso@redhat.com>
2012-06-12 17:46:07 +0100

    JBTM-1097 Use sensible names for generic transaction types

--- txframework/framework/src/main/java/org/jboss/narayana/txframework/impl/handlers/HandlerFactory.java
+++ txframework/framework/src/main/java/org/jboss/narayana/txframework/impl/handlers/HandlerFactory.java
@@ -2,8 +2,8 @@ package org.jboss.narayana.txframework.impl.handlers;
 
 import com.arjuna.mw.wst11.UserTransaction;
 import com.arjuna.mw.wst11.UserTransactionFactory;
-import org.jboss.narayana.txframework.api.annotation.transaction.AT;
-import org.jboss.narayana.txframework.api.annotation.transaction.BA;
+import org.jboss.narayana.txframework.api.annotation.transaction.Compensatable;
+import org.jboss.narayana.txframework.api.annotation.transaction.Transactional;
 import org.jboss.narayana.txframework.api.configuration.transaction.CompletionType;
 import org.jboss.narayana.txframework.api.exception.TXFrameworkException;
 import org.jboss.narayana.txframework.impl.handlers.restat.service.RESTATHandler;
@@ -18,9 +18,9 @@ public class HandlerFactory {
     public static ProtocolHandler createInstance(Object serviceImpl, Method serviceMethod) throws TXFrameworkException {
         Class serviceClass = serviceImpl.getClass();
 
-        BA BA = (BA) serviceClass.getAnnotation(BA.class);
-        if (BA != null) {
-            CompletionType completionType = BA.completionType();
+        Compensatable Compensatable = (Compensatable) serviceClass.getAnnotation(Compensatable.class);
+        if (Compensatable != null) {
+            CompletionType completionType = Compensatable.completionType();
             if (completionType == CompletionType.PARTICIPANT) {
                 return new WSBAParticipantCompletionHandler(serviceImpl, serviceMethod);
             } else if (completionType == CompletionType.COORDINATOR) {
@@ -30,8 +30,8 @@ public class HandlerFactory {
             }
         }
 
-        AT AT = (AT) serviceClass.getAnnotation(AT.class);
-        if (AT != null) {
+        Transactional Transactional = (Transactional) serviceClass.getAnnotation(Transactional.class);
+        if (Transactional != null) {
             if (isWSATTransactionRunning()) {
                 return new WSATHandler(serviceImpl, serviceMethod);
             } else // assume it must be a REST-AT transaction running.
