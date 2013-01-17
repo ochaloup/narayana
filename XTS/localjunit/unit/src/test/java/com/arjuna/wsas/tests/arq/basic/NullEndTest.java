168abcb4e89667079a07705aa918b6ecb293d21d
Amos Feng <zfeng@redhat.com>
2013-01-17 14:34:59 +0800

    JBTM-1355 merge xts localjunit tests

--- XTS/localjunit/unit/src/test/java/com/arjuna/wsas/tests/arq/basic/NullEndTest.java
+++ XTS/localjunit/unit/src/test/java/com/arjuna/wsas/tests/arq/basic/NullEndTest.java
@@ -1,6 +1,6 @@
 package com.arjuna.wsas.tests.arq.basic;
 
-import javax.inject.Inject;
+import static org.junit.Assert.fail;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -8,21 +8,31 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import com.arjuna.mw.wsas.UserActivity;
+import com.arjuna.mw.wsas.UserActivityFactory;
+import com.arjuna.mw.wsas.exceptions.NoActivityException;
 import com.arjuna.wsas.tests.WSASTestUtils;
 import com.arjuna.wsas.tests.arq.WarDeployment;
 
 @RunWith(Arquillian.class)
 public class NullEndTest {
-    @Inject
-    NullEnd test;
 
     @Deployment
     public static WebArchive createDeployment() {
-        return WarDeployment.getDeployment(NullEnd.class, WSASTestUtils.class);
+        return WarDeployment.getDeployment(WSASTestUtils.class);
     }
 
     @Test
-    public void test() throws Exception {
-        test.testNullEnd();
+    public void testNullEnd() throws Exception {
+        UserActivity ua = UserActivityFactory.userActivity();
+        try {
+            ua.end();
+            fail("should have thrown NoActivityException");
+        } catch (NoActivityException ex) {
+            // it's ok if we arrive here?
+        } catch (Exception ex) {
+            WSASTestUtils.cleanup(ua);
+            throw ex;
+        }
     }
 }
