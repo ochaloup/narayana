dde735f5527440be4d1f5b4a2c98711b38069d4e
Mark Little <mlittle@redhat.com>
2013-06-09 19:00:43 +0100

    Fixed test that didn't have any valid assertions.

--- ArjunaCore/txoj/tests/classes/com/hp/mwtests/ts/txoj/threadaction/ThreadActionTest.java
+++ ArjunaCore/txoj/tests/classes/com/hp/mwtests/ts/txoj/threadaction/ThreadActionTest.java
@@ -33,6 +33,8 @@ package com.hp.mwtests.ts.txoj.threadaction;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 import com.arjuna.ats.arjuna.coordinator.BasicAction;
 import com.hp.mwtests.ts.txoj.common.resources.BasicThreadedObject;
 
@@ -43,6 +45,8 @@ public class ThreadActionTest {
         BasicThreadedObject object2 = new BasicThreadedObject(false);
 
         System.out.println("Main thread has action " + BasicAction.Current());
+
+        assertEquals(BasicAction.Current(), null);
 
         object1.start();
         object2.start();
@@ -56,6 +60,7 @@ public class ThreadActionTest {
         }
 
         System.out.println("Main thread has action " + BasicAction.Current());
-    }
 
+        assertEquals(BasicAction.Current(), null);
+    }
 }
