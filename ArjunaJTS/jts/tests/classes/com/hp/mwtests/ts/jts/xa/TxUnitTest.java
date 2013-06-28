517cb08ac76ec2332920fe1ee9fdff9a0bf55bdf
Mark Little <mlittle@redhat.com>
2013-06-28 15:55:14 +0100

    JBTM-715

--- ArjunaJTS/jts/tests/classes/com/hp/mwtests/ts/jts/xa/TxUnitTest.java
+++ ArjunaJTS/jts/tests/classes/com/hp/mwtests/ts/jts/xa/TxUnitTest.java
@@ -41,9 +41,12 @@ import com.hp.mwtests.ts.jts.resources.TestBase;
 public class TxUnitTest extends TestBase {
     @Test
     public void test() throws Exception {
+        tx a = new tx();
+
         assertEquals(tx.tx_open(), tx.TX_OK);
         assertEquals(tx.tx_close(), tx.TX_OK);
         assertEquals(tx.tx_allow_nesting(), tx.TX_OK);
+        assertEquals(tx.tx_disable_nesting(), tx.TX_OK);
 
         assertEquals(tx.tx_set_transaction_timeout(10), tx.TX_OK);
         assertEquals(tx.tx_begin(), tx.TX_OK);
@@ -54,5 +57,26 @@ public class TxUnitTest extends TestBase {
         assertEquals(tx.tx_rollback(), tx.TX_OK);
 
         assertEquals(tx.tx_set_transaction_control(1), tx.TX_FAIL);
+    }
+
+    @Test
+    public void testNestingSuccess() throws Exception {
+        assertEquals(tx.tx_allow_nesting(), tx.TX_OK);
+        assertEquals(tx.tx_open(), tx.TX_OK);
+        assertEquals(tx.tx_begin(), tx.TX_OK);
+        assertEquals(tx.tx_begin(), tx.TX_OK);
+        assertEquals(tx.tx_commit(), tx.TX_OK);
+        assertEquals(tx.tx_rollback(), tx.TX_OK);
+        assertEquals(tx.tx_close(), tx.TX_OK);
+    }
+
+    @Test
+    public void testNestingFail() throws Exception {
+        assertEquals(tx.tx_disable_nesting(), tx.TX_OK);
+        assertEquals(tx.tx_open(), tx.TX_OK);
+        assertEquals(tx.tx_begin(), tx.TX_OK);
+        assertEquals(tx.tx_begin(), tx.TX_PROTOCOL_ERROR);
+        assertEquals(tx.tx_commit(), tx.TX_OK);
+        assertEquals(tx.tx_close(), tx.TX_OK);
     }
 }
