686000dc3afb244bbc4238d7ce43f6611df0be62
Tom Jenkinson <tom.jenkinson@redhat.com>
2013-10-21 14:43:09 +0100

    JBTM-1990 added more documentation to describe a possible FatalError if the store cannot be contacted

--- ArjunaCore/arjuna/classes/com/arjuna/ats/internal/arjuna/objectstore/jdbc/JDBCImple_driver.java
+++ ArjunaCore/arjuna/classes/com/arjuna/ats/internal/arjuna/objectstore/jdbc/JDBCImple_driver.java
@@ -42,6 +42,7 @@ import javax.naming.NamingException;
 
 import com.arjuna.ats.arjuna.common.ObjectStoreEnvironmentBean;
 import com.arjuna.ats.arjuna.common.Uid;
+import com.arjuna.ats.arjuna.exceptions.FatalError;
 import com.arjuna.ats.arjuna.exceptions.ObjectStoreException;
 import com.arjuna.ats.arjuna.logging.tsLogger;
 import com.arjuna.ats.arjuna.objectstore.StateStatus;
@@ -590,6 +591,9 @@ public abstract class JDBCImple_driver {
      * Set up the store for use.
      * 
      * @throws NamingException
+     * @throws {@link
+     *             FatalError} In case the configured store cannot be connected
+     *             to
      */
     public void initialise(final JDBCAccess jdbcAccess, String tableName,
             ObjectStoreEnvironmentBean jdbcStoreEnvironmentBean) throws SQLException, NamingException {
