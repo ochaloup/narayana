cb612c63106eb3a178a2ade70998446b06ee055c
Michael Musgrove <mmusgrov@redhat.com>
2013-11-19 16:37:43 +0000

    [JBTM-2031, JBTM-2020] Increase HttpURLConnection connection socket read timeout

--- rts/at/util/src/main/java/org/jboss/jbossts/star/util/TxSupport.java
+++ rts/at/util/src/main/java/org/jboss/jbossts/star/util/TxSupport.java
@@ -69,6 +69,8 @@ public class TxSupport {
      */
     public static final String TX_SEGMENT = "transaction-manager/";
 
+    public static final int DEFAULT_READ_TIMEOUT = 20000;
+
     private static int PORT = 8080;
     private static String BIND_ADDRESS = System.getProperty("jboss.bind.address", "localhost");
     private static String BASE_URL = "http://" + BIND_ADDRESS + ':';
@@ -87,7 +89,7 @@ public class TxSupport {
     private String body = null;
     private String contentType = null;
     private String txnMgr;
-    private int readTimeout = 5000;
+    private int readTimeout = DEFAULT_READ_TIMEOUT;
 
     public static void setTxnMgrUrl(String txnMgrUrl) {
         TXN_MGR_URL = txnMgrUrl;
@@ -98,7 +100,7 @@ public class TxSupport {
     }
 
     public TxSupport(String txnMgr) {
-        this(txnMgr, 5000);
+        this(txnMgr, DEFAULT_READ_TIMEOUT);
     }
 
     public TxSupport() {
