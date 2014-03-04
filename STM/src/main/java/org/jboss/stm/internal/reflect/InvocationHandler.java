31d3940b9433683d6f34114ae29482241ef0c685
Mark Little <mlittle@redhat.com>
2014-03-04 16:40:03 +0000

    Use setupStore to get stateStore from StateManager consistently.

--- STM/src/main/java/org/jboss/stm/internal/reflect/InvocationHandler.java
+++ STM/src/main/java/org/jboss/stm/internal/reflect/InvocationHandler.java
@@ -358,10 +358,13 @@ public class InvocationHandler<T> implements java.lang.reflect.InvocationHandler
                     _storeManager = new StoreManager(null, new TwoPhaseVolatileStore(new ObjectStoreEnvironmentBean()),
                             null);
                 } catch (final IllegalStateException ex) {
+                    _storeManager = null;
+
                     // means store already initialised so check to see if
                     // compatible
 
-                    if (StoreManager.getParticipantStore().getClass().equals(TwoPhaseVolatileStore.class)) {
+                    if (StoreManager.setupStore(null, ObjectModel.SINGLE).getClass()
+                            .equals(TwoPhaseVolatileStore.class)) {
                         // do nothing, as we are ok
                     } else {
                         ex.printStackTrace();
