1df9d6ac5d402b82f7afd12ebc12894f25a6a5ad
Tom Jenkinson <tom.jenkinson@redhat.com>
2014-06-02 23:45:55 +0100

    JBTM-2185 Re-enable CMR integration test

--- ArjunaJTA/jta/tests/classes/com/hp/mwtests/ts/jta/commitmarkable/integration/CMRIntegrationTest.java
+++ ArjunaJTA/jta/tests/classes/com/hp/mwtests/ts/jta/commitmarkable/integration/CMRIntegrationTest.java
@@ -119,8 +119,8 @@ public class CMRIntegrationTest {
         }
     }
 
-    private int threadCount = 100;
-    private int iterationCount = 2000;
+    private int threadCount = 20;
+    private int iterationCount = 50;
     private int waiting;
     private boolean go;
     private final Object waitLock = new Object();
