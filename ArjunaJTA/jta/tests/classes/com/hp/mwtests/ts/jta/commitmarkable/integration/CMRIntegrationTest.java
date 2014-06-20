d40dd50ceaf02c8d20b10094bf75d7d89b3e6807
Tom Jenkinson <tom.jenkinson@redhat.com>
2014-06-20 07:53:35 +0100

    JBTM-2195 reduce the iteration count for CMRIntegrationTest

--- ArjunaJTA/jta/tests/classes/com/hp/mwtests/ts/jta/commitmarkable/integration/CMRIntegrationTest.java
+++ ArjunaJTA/jta/tests/classes/com/hp/mwtests/ts/jta/commitmarkable/integration/CMRIntegrationTest.java
@@ -119,8 +119,8 @@ public class CMRIntegrationTest {
         }
     }
 
-    private int threadCount = 10;
-    private int iterationCount = 50;
+    private int threadCount = 5;
+    private int iterationCount = 20;
     private int waiting;
     private boolean go;
     private final Object waitLock = new Object();
