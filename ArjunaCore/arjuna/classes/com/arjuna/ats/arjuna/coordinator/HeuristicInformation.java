3ea9b5f138ef47fe593236f0d7a92fda8209fa08
Michael Musgrove <mmusgrov@redhat.com>
2015-08-07 11:08:11 +0100

    JBTM-2328 Deprecate HeuristicInformation

--- ArjunaCore/arjuna/classes/com/arjuna/ats/arjuna/coordinator/HeuristicInformation.java
+++ ArjunaCore/arjuna/classes/com/arjuna/ats/arjuna/coordinator/HeuristicInformation.java
@@ -32,12 +32,17 @@ package com.arjuna.ats.arjuna.coordinator;
  */
 
 /**
- * An implementation of this interface should be returned by an AbstractRecord
- * if it has caused a heuristic decision.
+ * An implementation of this interface should be returned by
+ * an AbstractRecord if it has caused a heuristic decision.
  *
- * @version $Id: HeuristicInformation.java 2342 2006-03-30 13:06:17Z $
+ * @version $Id: HeuristicInformation.java 2342 2006-03-30 13:06:17Z  $
  * @author Richard A. Begg (richard.begg@arjuna.com)
  */
+/**
+ * @deprecated as of 5.2.2.Final In a subsequent release we will be providing
+ *             this functionality via JMX MBeans
+ */
+@Deprecated
 public interface HeuristicInformation {
     /**
      * The type of heuristic.
