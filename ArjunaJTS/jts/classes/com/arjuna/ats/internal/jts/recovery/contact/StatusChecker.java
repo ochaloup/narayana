295f037227cf38c5ec34a9b5a138357372c86851
Michael Musgrove <mmusgrov@redhat.com>
2015-11-03 13:04:42 +0000

    JBTM-2547 StatusChecker reinterpretation of SystemException for Java idl orb

--- ArjunaJTS/jts/classes/com/arjuna/ats/internal/jts/recovery/contact/StatusChecker.java
+++ ArjunaJTS/jts/classes/com/arjuna/ats/internal/jts/recovery/contact/StatusChecker.java
@@ -277,6 +277,13 @@ public class StatusChecker {
                 // no transaction
             } catch (SystemException ex_corba) {
                 // why did this happen ?
+                if (ORBInfo.getOrbEnumValue() == ORBType.JAVAIDL) {
+                    // the original application has (probably) died
+                    if (jtsLogger.logger.isDebugEnabled()) {
+                        jtsLogger.logger.debug("StatusChecker.getStatus(" + transactionUid + ") - COMM_FAILURE = dead");
+                    }
+                    originalDead = true;
+                }
                 jtsLogger.i18NLogger.warn_recovery_contact_StatusChecker_11(ex_corba);
             } catch (Exception ex_other) {
                 // this really shouldn't happen
