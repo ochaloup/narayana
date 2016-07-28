e5dd695ef3f2fa8e3541c8e4052f5124d1a0772c
Tom Jenkinson <tom.jenkinson@redhat.com>
2016-07-28 16:09:35 +0100

    JBTM-2710 Updated to allow the JCA rollback call to invoke rollback on the XAResource for JTS (to match JTA)

--- ArjunaJTS/jts/classes/com/arjuna/ats/internal/jts/orbspecific/interposition/coordinator/ServerTransaction.java
+++ ArjunaJTS/jts/classes/com/arjuna/ats/internal/jts/orbspecific/interposition/coordinator/ServerTransaction.java
@@ -259,7 +259,7 @@ public class ServerTransaction extends ArjunaTransactionImple {
 
         org.omg.CosTransactions.Status s = get_status();
 
-        if ((s == org.omg.CosTransactions.Status.StatusCommitted)
+        if ((s == org.omg.CosTransactions.Status.StatusCommitted && preparedList.size() == 0)
                 || (s == org.omg.CosTransactions.Status.StatusRolledBack)) {
             int status = finalStatus();
 
