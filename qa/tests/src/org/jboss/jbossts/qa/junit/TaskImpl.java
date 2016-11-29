2bf9dd7fd4582b00a74222ff618f69cfaf538281
Tom Jenkinson <tom.jenkinson@redhat.com>
2016-11-29 15:08:39 +0000

    JBTM-2800 General qa/ upgrades to allow multiple additional QA_ARGS and to fail cleanly if process fails to start plus to store the upstream taskimpl separate to the users local one

--- qa/tests/src/org/jboss/jbossts/qa/junit/TaskImpl.java
+++ qa/tests/src/org/jboss/jbossts/qa/junit/TaskImpl.java
@@ -863,6 +863,12 @@ public class TaskImpl implements Task {
                     if ("Failed".equals(line)) {
                         printedFailed = true;
                     }
+                    if (line.startsWith("java.lang.OutOfMemoryError")) {
+                        throw new Exception("Test out of memory");
+                    }
+                    if (line.startsWith("Error occurred during initialization of VM")) {
+                        throw new Exception("Test out of memory");
+                    }
                     Date date = new Date();
                     out.println(simpleDateFormat.format(date) + " " + prefix + line);
                 }
@@ -874,14 +880,15 @@ public class TaskImpl implements Task {
                     printedReady.notify();
                 }
 
-                // if the process is explicitly destroyed we can see an
+                // // if the process is explicitly destroyed we can see an
                 // IOException because the output
-                // stream gets closed under the call to readLine(). shutdown is
-                // set befofe destroy
-                // is called so only trace the exception if we are not shut down
-                if (shutdown.get()) {
-                    return;
-                }
+                // // stream gets closed under the call to readLine(). shutdown
+                // is set befofe destroy
+                // // is called so only trace the exception if we are not shut
+                // down
+                // if (shutdown.get()) {
+                // return;
+                // }
                 out.printf("%s TaskReaderThread : exception before shutdown %s%n", getTaskPrefix(), e.getMessage());
                 System.out.printf("%s TaskReaderThread : exception before shutdown %s%n", getTaskPrefix(),
                         e.getMessage());
@@ -897,6 +904,9 @@ public class TaskImpl implements Task {
 
         public void shutdown() {
             shutdown.set(true);
+            synchronized (printedReady) {
+                printedReady.notifyAll();
+            }
         }
     }
     /**
