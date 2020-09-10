a8ace792bc949cc91b23fb17a7f3b6d759a1b1f3
Ondra Chaloupka <ochaloup@redhat.com>
2020-09-10 10:58:09 +0200

    JBTM-3343-lra-tests-on-all-runtimes-attept-#2

--- rts/lra/lra-test/lra-test-arquillian-extension/src/main/java/io/narayana/lra/arquillian/ConfigAuxiliaryArchiveAppender.java
+++ rts/lra/lra-test/lra-test-arquillian-extension/src/main/java/io/narayana/lra/arquillian/ConfigAuxiliaryArchiveAppender.java
@@ -23,7 +23,6 @@
 package io.narayana.lra.arquillian;
 
 import io.narayana.lra.arquillian.spi.NarayanaLRARecovery;
-import io.narayana.lra.client.internal.proxy.nonjaxrs.LRACDIExtension;
 import org.jboss.arquillian.container.test.spi.client.deployment.AuxiliaryArchiveAppender;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -40,25 +39,26 @@ public class ConfigAuxiliaryArchiveAppender implements AuxiliaryArchiveAppender 
     @Override
     public Archive<?> createAuxiliaryArchive() {
         JavaArchive archive = ShrinkWrap.create(JavaArchive.class)
-        // adding LRA spec interfaces under the Thorntail deployment
-                .addPackages(true, org.eclipse.microprofile.lra.annotation.Compensate.class.getPackage());
-        // adding Narayana LRA implementation under the Thorntail deployment
-        archive.addPackages(true, io.narayana.lra.client.NarayanaLRAClient.class.getPackage())
-                .addPackages(true, io.narayana.lra.Current.class.getPackage())
-                .addPackages(true, org.apache.http.HttpEntity.class.getPackage())
-                .addPackage(LRACDIExtension.class.getPackage())
-                .addAsResource("META-INF/services/javax.enterprise.inject.spi.Extension")
-                .addClass(org.jboss.weld.exceptions.DefinitionException.class)
-                .addAsManifestResource(new StringAsset("<beans version=\"1.1\" bean-discovery-mode=\"annotated\"></beans>"), "beans.xml");
+            // adding LRA spec interfaces under the Thorntail deployment
+            .addPackages(true, org.eclipse.microprofile.lra.annotation.Compensate.class.getPackage())
+            // adding Narayana LRA implementation under the Thorntail deployment
+            .addPackages(true, io.narayana.lra.client.NarayanaLRAClient.class.getPackage())
+            .addPackages(true, io.narayana.lra.Current.class.getPackage())
+            .addPackages(true, org.apache.http.HttpEntity.class.getPackage())
+            .addPackages(true, io.narayana.lra.client.internal.proxy.nonjaxrs.LRAParticipantRegistry.class.getPackage())
+            .addPackages(true, io.narayana.lra.client.internal.proxy.nonjaxrs.LRACDIExtension.class.getPackage())
+            .addAsResource("META-INF/services/javax.enterprise.inject.spi.Extension")
+            .addClass(org.jboss.weld.exceptions.DefinitionException.class)
+            .addAsManifestResource(new StringAsset("<beans version=\"1.1\" bean-discovery-mode=\"annotated\"></beans>"), "beans.xml");
 
         // adding Narayana LRA filters under the Thorntail deployment
         String filtersAsset = String.format("%s%n%s",
-                io.narayana.lra.filter.ClientLRAResponseFilter.class.getName(),
-                io.narayana.lra.filter.ClientLRARequestFilter.class.getName());
+            io.narayana.lra.filter.ClientLRAResponseFilter.class.getName(),
+            io.narayana.lra.filter.ClientLRARequestFilter.class.getName());
         archive.addPackages(true, io.narayana.lra.filter.ClientLRARequestFilter.class.getPackage())
-               .addAsResource(new StringAsset(filtersAsset), "META-INF/services/javax.ws.rs.ext.Providers")
-               .addAsResource(new StringAsset("org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder"),
-                    "META-INF/services/javax.ws.rs.client.ClientBuilder");
+            .addAsResource(new StringAsset(filtersAsset), "META-INF/services/javax.ws.rs.ext.Providers")
+            .addAsResource(new StringAsset("org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder"),
+                "META-INF/services/javax.ws.rs.client.ClientBuilder");
 
         // adding TCK required SPI implementations
         archive.addPackage(NarayanaLRARecovery.class.getPackage());
